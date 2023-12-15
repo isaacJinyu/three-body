47e00988125344723c02741f4058f58b6e8e572e
向劲宇 <137690584+isaacJinyu@users.noreply.github.com>
2023-12-15 14:50:20 +0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,150 @@
+# 导入pygame库
+import pygame
+import numpy as np
+# 初始化pygame
+pygame.init()
+# 设置窗口大小和标题
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Three-body problem')
+# 设置时钟
+clock = pygame.time.Clock()
+# 设置颜色
+black = (0, 0, 0)
+white = (255, 255, 255)
+# 设置字体
+font = pygame.font.SysFont('Arial', 32)
+# 定义万有引力常数
+G = 6.6743e-11 # m^3 kg^-1 s^-2
+# 定义缩放比例
+scale = 5e10 # m/px
+# 定义星体类
+class Star:
+    def __init__(self, m, r, v, color):
+        self.m = m # 质量
+        self.r = r # 位置
+        self.v = v # 速度
+        self.color = color # 颜色
+        self.trail = [] # 轨迹
+        self.max_trail = 100 # 最大轨迹长度
+    def update(self, dt):
+        # 更新位置
+        self.r += self.v * dt
+        # 添加轨迹点
+        self.trail.append(self.r.copy())
+        # 限制轨迹长度
+        if len(self.trail) > self.max_trail:
+            self.trail.pop(0)
+    def draw(self, offset):
+        # 绘制星体
+        pygame.draw.circle(screen, self.color, (self.r[0] / scale + 400 + offset[0], self.r[1] / scale + 300 + offset[1]), 5)
+        # 绘制轨迹
+        for i in range(len(self.trail) - 1):
+            pygame.draw.line(screen, self.color, (self.trail[i][0] / scale + 400 + offset[0], self.trail[i][1] / scale + 300 + offset[1]), (self.trail[i+1][0] / scale + 400 + offset[0], self.trail[i+1][1] / scale + 300 + offset[1]))
+    def apply_force(self, f):
+        # 应用力，计算加速度
+        a = f / self.m
+        # 更新速度
+        self.v += a * dt
+# 定义一个函数，生成随机的星体
+def random_star():
+    # 设置随机的质量、位置和速度的范围
+    m_min = 1e30 # kg
+    m_max = 5e31 # kg
+    r_min = -3e13 # m
+    r_max = 3e13 # m
+    v_min = -1e5 # m/s
+    v_max = 1e5 # m/s
+    # 生成随机的质量、位置和速度
+    m = np.random.uniform(m_min, m_max)
+    r = np.random.uniform(r_min, r_max, size=3)
+    v = np.random.uniform(v_min, v_max, size=3)
+    # 生成随机的颜色
+    color = tuple(np.random.randint(0, 256, size=3))
+    # 创建星体对象并返回
+    return Star(m, r, v, color)
+# 创建一个空列表，用于存储星体对象
+stars = [Star(5e33, 0, 0, (255, 0, 0))]
+# 定义要生成的星体的数量
+n = 10
+# 用一个循环，生成随机的星体，并添加到列表中
+for i in range(n):
+    stars.append(random_star())
+# 定义一个函数，计算两个星体之间的引力
+def gravity(s1, s2):
+    # 计算相对位置和距离
+    r = s2.r - s1.r
+    d = np.linalg.norm(r)
+    # 计算引力
+    f = G * s1.m * s2.m * r / d**3
+    # 返回结果
+    return f
+# 定义一个函数，计算所有星体之间的引力，并更新速度和位置
+def update_system(dt):
+    # 用一个双重循环，遍历所有星体的组合
+    for i in range(n):
+        for j in range(i+1, n):
+            # 计算两个星体之间的引力
+            f = gravity(stars[i], stars[j])
+            # 应用引力，更新速度和位置
+            stars[i].apply_force(f)
+            stars[j].apply_force(-f)
+    # 用一个循环，更新所有星体的位置
+    for star in stars:
+        star.update(dt)
+# 定义一个函数，绘制所有星体和文字
+def draw_system():
+    # 填充背景色
+    screen.fill(black)
+    # 计算星体1相对于屏幕中心的偏移量
+    offset = -stars[0].r / scale
+    # 用一个循环，绘制所有星体
+    for star in stars:
+        star.draw(offset)
+    # 绘制文字
+    # text = font.render('Press WASD to control star 1', True, white)
+    # screen.blit(text, (10, 10))
+    # 更新屏幕
+    pygame.display.flip()
+# 定义一个函数，处理键盘事件
+def handle_key(event):
+    # 定义键盘控制的加速度
+    acc = 1e-1 # m/s^2
+    # 判断按下的键
+    if event.key == pygame.K_w:
+        # 按下W键，给星体1施加向上的加速度
+        stars[0].apply_force(np.array([0.0, -acc, 0.0]) * stars[0].m)
+    elif event.key == pygame.K_s:
+        # 按下S键，给星体1施加向下的加速度
+        stars[0].apply_force(np.array([0.0, acc, 0.0]) * stars[0].m)
+    elif event.key == pygame.K_a:
+        # 按下A键，给星体1施加向左的加速度
+        stars[0].apply_force(np.array([-acc, 0.0, 0.0]) * stars[0].m)
+    elif event.key == pygame.K_d:
+        # 按下D键，给星体1施加向右的加速度
+        stars[0].apply_force(np.array([acc, 0.0, 0.0]) * stars[0].m)
+    elif event.key == pygame.K_i:
+        # 按下I键，给星体1施加向前的加速度
+        stars[0].apply_force(np.array([0.0, 0.0, acc]) * stars[0].m)
+    elif event.key == pygame.K_k:
+        # 按下K键，给星体1施加向后的加速度
+        stars[0].apply_force(np.array([0.0, 0.0, -acc]) * stars[0].m)
+# 定义一个循环，主要逻辑
+running = True
+while running:
+    # 设置时间步长
+    dt = clock.tick(300)*5e4 # s
+    # 处理事件
+    for event in pygame.event.get():
+        # 判断事件类型
+        if event.type == pygame.QUIT:
+            # 点击关闭按钮，退出循环
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            # 按下键盘，处理键盘事件
+            handle_key(event)
+    # 更新系统
+    update_system(dt)
+    # 绘制系统
+    draw_system()
+# 退出pygame
+pygame.quit()
---
+++ three_body1.py
@@ -0,0 +1,134 @@
+# 导入numpy和matplotlib库
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import animation
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+# 定义万有引力常数
+G = 6.6743e-11 # m^3 kg^-1 s^-2
+
+# 定义三颗星的质量，位置和速度
+# 参考《三体》中的设定，假设三颗星的质量相等，初始位置在一个正三角形的顶点，初始速度垂直于位置矢量
+m1 = m2 = m3 = 2e30 # kg
+r1 = np.array([1.0, 0.0, 0.0]) * 1.5e11 # m
+r2 = np.array([-0.5, np.sqrt(3)/2, 0.0]) * 1.5e11 # m
+r3 = np.array([-0.5, -np.sqrt(3)/2, 0.0]) * 1.5e11 # m
+v1 = np.array([0.0, 1.02, 0.05]) * 3e4 # m/s
+v2 = np.array([-np.sqrt(3)/2, -0.5, 0.0]) * 3e4 # m/s
+v3 = np.array([np.sqrt(3)/2, -0.5, 0.0]) * 3e4 # m/s
+
+# 定义模拟的时间参数
+t_start = 0 # s
+t_end = 3e8 # s
+dt = 1e5 # s
+t = np.arange(t_start, t_end, dt) # 时间数组
+n = len(t) # 时间步数
+
+# 定义一个函数，根据当前的位置和速度，计算下一时刻的位置和速度
+def update_system(r1, r2, r3, v1, v2, v3, dt):
+    # 计算三颗星之间的相对位置和距离
+    r12 = r2 - r1
+    r13 = r3 - r1
+    r23 = r3 - r2
+    d12 = np.linalg.norm(r12)
+    d13 = np.linalg.norm(r13)
+    d23 = np.linalg.norm(r23)
+    # 计算三颗星之间的引力
+    f12 = G * m1 * m2 * r12 / d12**3
+    f13 = G * m1 * m3 * r13 / d13**3
+    f23 = G * m2 * m3 * r23 / d23**3
+    # 计算三颗星的加速度
+    a1 = (f12 + f13) / m1
+    a2 = (-f12 + f23) / m2
+    a3 = (-f13 - f23) / m3
+    # 计算下一时刻的速度
+    v1_next = v1 + a1 * dt
+    v2_next = v2 + a2 * dt
+    v3_next = v3 + a3 * dt
+    # 计算下一时刻的位置
+    r1_next = r1 + v1_next * dt
+    r2_next = r2 + v2_next * dt
+    r3_next = r3 + v3_next * dt
+    # 返回结果
+    return r1_next, r2_next, r3_next, v1_next, v2_next, v3_next
+
+# 定义一个数组，用来存储每一时刻的位置和速度
+r = np.zeros((n, 3, 3)) # 第一维是时间，第二维是星体，第三维是坐标
+v = np.zeros((n, 3, 3)) # 第一维是时间，第二维是星体，第三维是速度分量
+r[0, 0, :] = r1
+r[0, 1, :] = r2
+r[0, 2, :] = r3
+v[0, 0, :] = v1
+v[0, 1, :] = v2
+v[0, 2, :] = v3
+
+# 循环计算每一时刻的位置和速度
+for i in range(n-1):
+    # 使用返回值来更新位置和速度
+    r1, r2, r3, v1, v2, v3 = update_system(r1, r2, r3, v1, v2, v3, dt)
+    r[i+1, 0, :] = r1
+    r[i+1, 1, :] = r2
+    r[i+1, 2, :] = r3
+    v[i+1, 0, :] = v1
+    v[i+1, 1, :] = v2
+    v[i+1, 2, :] = v3
+
+
+# 绘制三维动画
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# 去掉坐标轴
+ax.set_axis_off()
+# 设置坐标轴比例
+ax.set_box_aspect((1, 1, 1))
+ax.set_xlim(-1e12, 1e12)
+ax.set_ylim(-1e12, 1e12)
+ax.set_zlim(-1e12, 1e12)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.set_title('Three-body problem')
+line1, = ax.plot([], [], [], 'r-')
+line2, = ax.plot([], [], [], 'b-')
+line3, = ax.plot([], [], [], 'g-')
+point1, = ax.plot([], [], [], 'ro')
+point2, = ax.plot([], [], [], 'bo')
+point3, = ax.plot([], [], [], 'go')
+
+
+def animate(i):
+    line1.set_data(r[:i+1, 0, 0], r[:i+1, 0, 1])
+    line1.set_3d_properties(r[:i+1, 0, 2])
+    line2.set_data(r[:i+1, 1, 0], r[:i+1, 1, 1])
+    line2.set_3d_properties(r[:i+1, 1, 2])
+    line3.set_data(r[:i+1, 2, 0], r[:i+1, 2, 1])
+    line3.set_3d_properties(r[:i+1, 2, 2])
+    point1.set_data([r[i, 0, 0]], [r[i, 0, 1]])
+    point1.set_3d_properties([r[i, 0, 2]])
+    point2.set_data([r[i, 1, 0]], [r[i, 1, 1]])
+    point2.set_3d_properties([r[i, 1, 2]])
+    point3.set_data([r[i, 2, 0]], [r[i, 2, 1]])
+    point3.set_3d_properties([r[i, 2, 2]])
+
+    x1, y1, z1 = r[i, 0, :]  # 获取第一个点的位置
+    ax.set_xlim(x1 - 1e12, x1 + 1e12)  # 设置 x 轴的范围
+    ax.set_ylim(y1 - 1e12, y1 + 1e12)  # 设置 y 轴的范围
+    ax.set_zlim(z1 - 1e12, z1 + 1e12)  # 设置 z 轴的范围
+    ax.view_init(elev=20, azim=i / 5)  # 设置视角
+
+    # 清除旧的标签
+    for text in ax.texts:
+        text.remove()
+
+    ax.text(r[i, 0, 0], r[i, 0, 1], r[i, 0, 2], f'v1 = {np.linalg.norm(v[i, 0, :]):.1f} m/s', color='r', fontsize=10, horizontalalignment='left', verticalalignment='top')
+    ax.text(r[i, 1, 0], r[i, 1, 1], r[i, 1, 2], f'v2 = {np.linalg.norm(v[i, 1, :]):.1f} m/s', color='b', fontsize=10, horizontalalignment='left', verticalalignment='top')
+    ax.text(r[i, 2, 0], r[i, 2, 1], r[i, 2, 2], f'v3 = {np.linalg.norm(v[i, 2, :]):.1f} m/s', color='g', fontsize=10, horizontalalignment='left', verticalalignment='top')
+    # 返回结果
+    return line1, line2, line3, point1, point2, point3
+
+anim = animation.FuncAnimation(fig, animate, frames=n, interval=0.5)
+
+plt.show()
